6cb177bf12dafb98ccc2f3659a498bbcc2adb1bc
Glitch (hello-webpage) <none>
2020-03-23 22:14:32 +0000

    🍥🌞 Checkpoint ./script.js:2585035/593

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+String.prototype.escapeDiacritics = function()
+{
+    return this.replace(/ą/g, 'a').replace(/Ą/g, 'A')
+        .replace(/ć/g, 'c').replace(/Ć/g, 'C')
+        .replace(/ę/g, 'e').replace(/Ę/g, 'E')
+        .replace(/ł/g, 'l').replace(/Ł/g, 'L')
+        .replace(/ń/g, 'n').replace(/Ń/g, 'N')
+        .replace(/ó/g, 'o').replace(/Ó/g, 'O')
+        .replace(/ś/g, 's').replace(/Ś/g, 'S')
+        .replace(/ż/g, 'z').replace(/Ż/g, 'Z')
+        .replace(/ź/g, 'z').replace(/Ź/g, 'Z');
+}
+
+function adddate()
+{
+  
+}
+
 function dconvert()
 {
   var date = document.getElementById("dateinput").value.split('\n');
   var results = "Nazwa użytkownika,Imię,Nazwisko,Nazwa wyświetlana\n";
   
-  for 
+  
   document.getElementById("dateoutput").innerHTML = results + date[4] + date[3];
-   
+  
+  
+  
 }
 
 function dpaste()
