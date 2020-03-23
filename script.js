367bf7701acf97404ebd0f35f099353542306d94
Glitch (hello-webpage) <none>
2020-03-23 22:02:32 +0000

    🚊🍄 Checkpoint ./script.js:2585035/128

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function dconvert()
 {
-   var date = document.getElementById("dateinput").value;
-  document.getElementById("dateoutput").innerHTML = date
+  var date = document.getElementById("dateinput").value.split('\n');
+  var results = "Nazwa użytkownika,Imię,Nazwisko,Nazwa wyświetlana\n";
+  
+  for 
+  document.getElementById("dateoutput").innerHTML = results + date[4] + date[3];
    
 }
 
