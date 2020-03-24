02a9187b95543db523b167de3278046e12227a06
Glitch (hello-webpage) <none>
2020-03-24 18:26:05 +0000

    🦀📷 Checkpoint ./scripts/csvconvert.js:2585035/6157 ./index.html:2585035/2470 ./css/style.css:2585035/1360

--- scripts/csvconvert.js
+++ scripts/csvconvert.js
@@ -1,53 +1,66 @@
-document.designMode = 'on';
+document.designMode = "on";
 
-var results = "Nazwa użytkownika,Imię,Nazwisko,Nazwa wyświetlana\n";
+var results = "";
 
-String.prototype.escapeDiacritics = function()
-{
-    return this.replace(/ą/g, 'a').replace(/Ą/g, 'A')
-        .replace(/ć/g, 'c').replace(/Ć/g, 'C')
-        .replace(/ę/g, 'e').replace(/Ę/g, 'E')
-        .replace(/ł/g, 'l').replace(/Ł/g, 'L')
-        .replace(/ń/g, 'n').replace(/Ń/g, 'N')
-        .replace(/ó/g, 'o').replace(/Ó/g, 'O')
-        .replace(/ś/g, 's').replace(/Ś/g, 'S')
-        .replace(/ż/g, 'z').replace(/Ż/g, 'Z')
-        .replace(/ź/g, 'z').replace(/Ź/g, 'Z');
-}
+String.prototype.escapeDiacritics = function() {
+  return this.replace(/ą/g, "a")
+    .replace(/Ą/g, "A")
+    .replace(/ć/g, "c")
+    .replace(/Ć/g, "C")
+    .replace(/ę/g, "e")
+    .replace(/Ę/g, "E")
+    .replace(/ł/g, "l")
+    .replace(/Ł/g, "L")
+    .replace(/ń/g, "n")
+    .replace(/Ń/g, "N")
+    .replace(/ó/g, "o")
+    .replace(/Ó/g, "O")
+    .replace(/ś/g, "s")
+    .replace(/Ś/g, "S")
+    .replace(/ż/g, "z")
+    .replace(/Ż/g, "Z")
+    .replace(/ź/g, "z")
+    .replace(/Ź/g, "Z");
+};
 
-function addDate(line)
-{
+function addDate(line) {
   var name = line.split(",");
   var domena = document.getElementById("domena").value;
-  if (domena[0] != '@')
-    {
-      domena = '@' + domena;
-    }
+  if (domena[0] != "@") {
+    domena = "@" + domena;
+  }
   var username = name[0] + name[1] + domena;
   username = username.escapeDiacritics();
-  username = username.replace('-', '');
-  results = results + username.toLowerCase() + ',' + name[0] + ',' + name[1] + ',' + name[0] + ' ' + name[1]  + '\n'; 
+  username = username.replace("-", "");
+  results =
+    results +
+    username.toLowerCase() +
+    "," +
+    name[0] +
+    "," +
+    name[1] +
+    "," +
+    name[0] +
+    " " +
+    name[1] +
+    "\n";
 }
 
-function dconvert()
-{
-  results = "";
-  var date = document.getElementById("dateinput").value.split('\n');
+function dconvert() {
+  results = "Nazwa użytkownika,Imię,Nazwisko,Nazwa wyświetlana,Stanowisko,Dział,Numer biura,Telefon w biurze,Telefon komórkowy,Numer faksu,Adres,Miejscowość,Województwo,Kod pocztowy,Kraj lub region\n";
+  var date = document.getElementById("dateinput").value.split("\n");
   date.forEach(addDate);
-  var date = document.getElementById("dateoutput").innerHTML = results;
-  
+  var date = (document.getElementById("dateoutput").innerHTML = results);
 }
 
-function dpaste()
-{
+function dpaste() {
   var date = document.getElementById("dateinput");
   date.select();
-  document.execCommand('paste');
+  document.execCommand("paste");
 }
 
-function dcopy()
-{
-   var csvdate = document.getElementById("dateoutput");
+function dcopy() {
+  var csvdate = document.getElementById("dateoutput");
   csvdate.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
 }
